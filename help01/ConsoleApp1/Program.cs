afaf8830008b97b3a65b9b72ffa80baba8dda18a
Unripe01 <dalmore.name@gmail.com>
2021-06-01 00:23:00 +0900

    first commit

---
+++ help01/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ファイルを想定
+            const string FILE = 
+@"1234567890
+ABCDEFGHIJ";
+
+            //ハッシュテーブルを想定
+            var myHash = new HashCode();
+
+            //ROW
+            var f = FILE.Split();
+
+
+
+            //COLUMN（item）
+        }
+    }
+}
